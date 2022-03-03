e73cde352881854a44f6f90115f184cebae16c56
Jyotsna7489 <doddapaneni.jyotsna@gmail.com>
2022-03-03 14:12:46 -0600

    test commit

---
+++ Main.groovy
@@ -0,0 +1,8 @@
+def name = "Matthew"
+int programmingPoints = 10
+
+println "Hello ${name}"
+println "${name} has at least ${programmingPoints} programming points."
+println "${programmingPoints} squared is ${Square.square(programmingPoints)}"
+println "${programmingPoints} divided by 2 bonus points is ${Division.divide(programmingPoints, 2)}"
+println "${programmingPoints} plus 3 bonus points is ${Sum.sum(programmingPoints, 3)}"
